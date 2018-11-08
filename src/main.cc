82413827abcce39b134247cee3f1c74b7302a226
cm_ramosx <you@example.com>
2018-11-08 09:27:44 -0800

    here you go!

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include "windchillindex.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using edu::vcccd::vc::csv13::computeWindChillIndex;
+
+
+
+int main(int argc, char  *argv[])
+{
+    cout << "The temperature is currently " << computeWindChillIndex(9.0,2.0) << endl;
+
+    return 0;
+}
